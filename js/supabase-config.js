d16efc2a4fed4e144f800c5c7919a22e667539a9
Rajankumar-cm <technicalrajan73@gmail.com>
2026-06-02 11:09:52 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,6 @@
-// =============================================
-// SUPABASE CONFIGURATION
-// =============================================
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
 const SUPABASE_URL = 'https://djohisxyahugicokwpox.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqb2hpc3h5YWh1Z2ljb2t3cG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNDg3MjYsImV4cCI6MjA5NTkyNDcyNn0.yw1ouxc9eIPAU_ZO1GMHUpZ4bW6ddzrwllb4GmtrVjI';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
