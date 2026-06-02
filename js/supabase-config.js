ee740c8ccfda190e1d7a3425b12e1908a535700d
Rajankumar-cm <technicalrajan73@gmail.com>
2026-06-02 09:46:18 +0530

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-// =============================================
-// SUPABASE CONFIGURATION
-// =============================================
-const SUPABASE_URL = 'https://djohisxyahugicokwpox.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqb2hpc3h5YWh1Z2ljb2t3cG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNDg3MjYsImV4cCI6MjA5NTkyNDcyNn0.yw1ouxc9eIPAU_ZO1GMHUpZ4bW6ddzrwllb4GmtrVjI';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
